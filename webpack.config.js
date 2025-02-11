971e2428e7f128ea7662b263af64943aefcf837b
sdkn104 <sdkn104@gmail.com>
2025-02-11 21:35:29 +0900

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,11 @@
-$ npm i puppeteer-core node-clipboardy
+$const path = require('path');
+
+module.exports = {
+  entry: './startgai.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+};
+
