6f8da5178dfec466ffd8124f65f827930db9738e
sdkn104 <sdkn104@gmail.com>
2026-07-10 20:17:44 +0900

    env.cmd の削除

--- env.cmd
+++
@@ -1,8 +0,0 @@
-REM @echo off
-
-call "C:\Users\QP48568\Downloads\NoSync\node-v22.13.0-win-x64\node_env.cmd"
-REM call "C:\Users\QP48568\Downloads\NoSync\node-v14.16.0-win-x64\node_env.cmd"
-REM call "C:\Users\QP48568\Downloads\NoSync\node-v22.13.0-win-x64\set_proxy.bat"
-
-cmd
-
